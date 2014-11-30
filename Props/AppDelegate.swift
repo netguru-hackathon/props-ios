270e3261c04587f2357eb1a0cb00efafadb80ed6
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2014-11-30 20:50:55 +0100

    Initial project commit

---
+++ Props/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//
+//  Copyright (c) 2014 Netguru Sp. z o.o. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain internal class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    /// The application's main window.
+    @IBOutlet internal var window: UIWindow!
+
+}
